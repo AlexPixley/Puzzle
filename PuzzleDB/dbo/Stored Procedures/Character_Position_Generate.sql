64cc0a12d77adf2e46bc2fe77e9f1c208bf857d6
AlexPixley <44739214+AlexPixley@users.noreply.github.com>
2018-11-04 00:05:51 -0400

    Initial upload

---
+++ PuzzleDB/dbo/Stored Procedures/Character_Position_Generate.sql
@@ -0,0 +1,48 @@
+﻿
+/*=========================================================================================
+== Purpose: 
+== 2018-04-01 ap: Created procedure
+== 
+== EXEC dbo.Character_Position_Generate @Across = 6, @Down = 6
+=========================================================================================*/
+
+CREATE PROCEDURE [dbo].[Character_Position_Generate] @Across SMALLINT,
+                                                     @Down SMALLINT
+AS
+	BEGIN
+
+	    IF OBJECT_ID('tempdb..#Character_Position') IS NOT NULL
+		   DROP TABLE #Character_Position;
+
+	    CREATE TABLE #Character_Position
+	    ([Character] INT IDENTITY(1, 1) PRIMARY KEY CLUSTERED ,
+		Across     SMALLINT,
+		Down     SMALLINT
+	    );
+
+	    DECLARE @A SMALLINT = 1, @D SMALLINT = 1;
+
+	    WHILE @A <= @Across
+			AND @D <= @Down
+		   BEGIN
+
+			  INSERT INTO #Character_Position
+			  SELECT Across = @A,
+				    Down = @D;
+
+			  IF @D = @Down
+				 BEGIN
+					SET @D = 1;
+					SET @A = @A + 1;
+			  END;
+				 ELSE
+				 BEGIN
+					SET @D = @D + 1;
+			  END;
+		   END;
+
+	    SELECT [Character],
+			 Across,
+			 Down
+	    FROM #Character_Position;
+	END;
---
+++ PuzzleDB/dbo/Stored Procedures/Puzzle_Insert.sql
@@ -0,0 +1,28 @@
+﻿
+/*=========================================================================================
+== Purpose: 
+== 2018-04-01 ap: Created procedure
+== 
+== EXEC dbo.Puzzle_Insert @PuzzleDescription = 'Test', @Across = 11, @Down = 11
+=========================================================================================*/
+
+CREATE PROCEDURE [dbo].[Puzzle_Insert] @PuzzleDescription VARCHAR(50),
+						    @Across           TINYINT,
+						    @Down           TINYINT,
+						    @PuzzleScope       INT         = NULL OUTPUT
+AS
+	BEGIN
+
+	    INSERT INTO dbo.Puzzle
+	    (PuzzleDescription,
+		Across,
+		Down
+	    )
+	    VALUES
+	    ( @PuzzleDescription,
+		 @Across,
+		 @Down
+	    );
+
+	    SET @PuzzleScope = SCOPE_IDENTITY();
+	END;
