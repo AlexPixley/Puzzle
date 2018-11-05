f3f4121efe200e74334e9ee69124712ceb5ce2c4
Alex <Alex@BHODI>
2018-11-04 20:44:25 -0500

    Continuing work.

--- PuzzleDB/dbo/Tables/SignalToNoise.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[SignalToNoise] (
-    [PuzzleId] INT       NOT NULL,
-    [Across]   INT       NOT NULL,
-    [Down]     INT       NOT NULL,
-    [IsOpen]   BIT       NOT NULL,
-    [Char]     NCHAR (1) NOT NULL,
-    CONSTRAINT [PK_SignalToNoise] PRIMARY KEY CLUSTERED ([PuzzleId] ASC, [Across] ASC, [Down] ASC)
-);
-
